041e2769b79ff4485e457a18b3d18089e16990b4
rbenitec <105337433+rbenitec@users.noreply.github.com>
2024-06-06 01:37:11 -0500

    update log

--- src/main/java/com/parking/service/login/controller/LoginController.java
+++ src/main/java/com/parking/service/login/controller/LoginController.java
@@ -6,12 +6,13 @@ import com.parking.service.login.controller.dto.ResponseDto;
 import com.parking.service.login.service.business.AccountService;
 import com.parking.service.login.service.business.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequestMapping("/login")
 @RequiredArgsConstructor
@@ -22,11 +23,13 @@ public class LoginController {
 
     @PostMapping("/authenticate")
     public ResponseEntity<ResponseDto> authenticationCustomer(@RequestBody RequestDto request) {
+        log.info("Call Autentication Customer: {}", request.toString());
         return ResponseEntity.ok(authenticationService.userAuthentication(request));
     }
 
     @PostMapping("/account-created")
     public ResponseEntity<ResponseDto> accountCreated(@RequestBody RequestAccountDto requestAccount) {
+        log.info("Call Created Customer: {}", requestAccount.toString());
         ResponseDto response = accountService.createdAccount(requestAccount);
         if(response!=null) {
             return ResponseEntity.ok(response);
